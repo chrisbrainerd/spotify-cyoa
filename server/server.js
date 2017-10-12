083450985021d4940b67da6c6b2f7829289657d6
chrisbrainerd <christopher.brainerd@gmail.com>
2017-10-11 23:12:24 -0400

    initial commit

--- server/server.js
+++ server/server.js
@@ -5,13 +5,26 @@ const compression = require("compression");
 const path = require("path");
 const logger = require("./middleware/logger");
 const { devMiddleware, hotMiddleware } = require("./middleware/webpack");
-
+const cookieParser = require('cookie-parser');
+const querystring = require('querystring');
+const request = require('request');
 const app = express();
+
+const client_id = 'ff1aab853bf64c7e87a91f82621260fc'; // Your client id
+const client_secret = '795dd0cd2d5f4ffe99036ca79ca2a09c'; // Your secret
+const redirect_uri = 'http://localhost:8080/callback'; // Your redirect uri
+const scopes = 'user-read-private user-read-email'; // space-delimited spotify acct scopes
+let stateKey = 'spotify_auth_state';
+
+
 
 app.set("x-powered-by", false);
 
 app.use(compression());
 app.use(logger);
+app.use(express.static(__dirname + '/public'))
+  .use(cookieParser());
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
@@ -20,7 +33,18 @@ if (process.env.NODE_ENV === "production") {
   app.use(hotMiddleware);
 }
 
-app.get("*", (req, res) => {
+var generateRandomString = function (length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+
+app.get("/", (req, res) => {
   if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve("build", "index.html"));
   } else {
@@ -28,6 +52,112 @@ app.get("*", (req, res) => {
     res.end();
   }
 });
+app.get('/login', function (req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+app.get('/callback', function (req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function (error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function (req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
+
+
+
 
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log("Express started at http://localhost:%d\n", server.address().port);
@@ -35,3 +165,5 @@ const server = app.listen(process.env.PORT || 8080, () => {
     console.log("Waiting for webpack...\n");
   }
 });
+
+
--- src/common/App/App.jsx
+++ src/common/App/App.jsx
@@ -1,15 +1,88 @@
 import React, { Component } from "react";
 import cls from "./App.css";
+require('./App.css');
 
 class App extends Component {
-  state = {
-    text: "Hello World!",
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "Hello World!",
+      loggedIn: false,
+      display_name: undefined,
+      id: undefined,
+      email: undefined,
+      external_urls: { spotify: undefined },
+      href: undefined,
+      images: [{ url: undefined }]
+    };
+  }
+
+  componentDidMount() {
+    var params = this.getHashParams();
+    var access_token = params.access_token,
+      refresh_token = params.refresh_token,
+      error = params.error;
+    if (error) {
+      alert('There was an error during the authentication');
+    } else {
+      if (access_token) {
+        // render oauth info
+        // oauthPlaceholder.innerHTML = oauthTemplate({
+        //   access_token: access_token,
+        //   refresh_token: refresh_token
+        // });
+        const fetchOptions = {
+          method: 'GET',
+          headers: {
+            'Authorization': 'Bearer ' + access_token
+          }
+        };
+        fetch('https://api.spotify.com/v1/me', fetchOptions).then((response) => response.json())
+          .then((data) => {
+            const state = Object.assign(data, { loggedIn: true });
+            this.setState(state);
+          })
+      } else {
+
+      }
+    }
+
+  }
+
+  getHashParams = () => {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while (e = r.exec(q)) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
 
   render() {
+    const { display_name, images, id, email, external_urls, href } = this.state;
     return (
       <div>
-        <h1 className={cls.title}>{this.state.text}</h1>
+        <div id="login">
+          <h1>First, log in to spotify</h1>
+          <a href="/login">Log in</a>
+        </div>
+        {this.state.loggedIn &&
+          <div>
+            <h1>Logged in as {display_name}</h1>
+            <img id="avatar" width="200" src={images[0].url} />
+            <dl>
+              <dt>Display name</dt><dd>{display_name}</dd>
+              <dt>Username</dt><dd>{id}</dd>
+              <dt>Email</dt><dd>{email}</dd>
+              <dt>Spotify URI</dt><dd><a href={external_urls.spotify}>{external_urls.spotify}</a></dd>
+              <dt>Link</dt><dd><a href={href}>{href}</a></dd>
+              <dt>Profile Image</dt><dd>{images[0].url}</dd>
+            </dl>
+            <p><a href="/">Log in again</a></p>
+          </div>
+        }
       </div>
     );
   }
